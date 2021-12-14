29cbfcb8535b16e9ed7ecaa12e6737edbea50d7d
Nguyễn Đăng Kỳ <87688740+NDK2509@users.noreply.github.com>
2021-12-15 01:49:15 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
-const modalStart = new bootstrap.Modal(document.getElementById("modal-start"));
+const modalStart = new bootstrap.Modal(
+    document.getElementById("modal-start"));
 modalStart.show();
-const modalGift = new bootstrap.Modal(document.getElementById("modal-gift"));
-
+const modalGift = new bootstrap.Modal(
+    document.getElementById("modal-gift"));
+const modalSnower = new bootstrap.Modal(
+	document.getElementById("modal-snower"));
 document.getElementById("gift2").addEventListener("click", () => {
-    modalGift.show();
+	modalGift.show();
 });
+document.getElementById("snower").addEventListener("click", () => {
+	modalSnower.show();
+});
+document.getElementById('btn-close-mStart').addEventListener('clcik', () => {
+    
+})
