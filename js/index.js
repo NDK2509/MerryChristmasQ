c74411eceb4363a9582f056a5841961948d2da9d
Nguyễn Đăng Kỳ <87688740+NDK2509@users.noreply.github.com>
2021-12-18 17:22:35 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,16 +1,33 @@
-const modalStart = new bootstrap.Modal(
-    document.getElementById("modal-start"));
+const CLICK_EVENT = "click";
+
+const modalStart = new bootstrap.Modal(document.getElementById("modal-start"));
 modalStart.show();
-const modalGift = new bootstrap.Modal(
-    document.getElementById("modal-gift"));
+const modalGift = new bootstrap.Modal(document.getElementById("modal-gift"));
 const modalSnower = new bootstrap.Modal(
-	document.getElementById("modal-snower"));
-document.getElementById("gift2").addEventListener("click", () => {
+	document.getElementById("modal-snower")
+);
+
+document.getElementById("gift2").addEventListener(CLICK_EVENT, () => {
 	modalGift.show();
 });
-document.getElementById("snower").addEventListener("click", () => {
+document.getElementById("snower").addEventListener(CLICK_EVENT, () => {
 	modalSnower.show();
 });
-document.getElementById('btn-close-mStart').addEventListener('clcik', () => {
-    
-})
+//var sound = new Howl({
+//	src: ['sound/bg-sound.mp3'],
+//	autoplay: true,
+////loop: true,
+//	volume: 0.3
+//});
+var soundBell = new Howl({
+	src: ["sound/bellsound.mp3"],
+});
+var meowSound = new Howl({
+	src: ["sound/meowsound.mp3"],
+});
+document.getElementById("bell").addEventListener(CLICK_EVENT, () => {
+	soundBell.play();
+});
+document.getElementById("meow").addEventListener(CLICK_EVENT, () => {
+	meowSound.play();
+});
